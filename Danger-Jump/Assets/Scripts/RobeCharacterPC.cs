9605473d10bb920072a29b445753df4bbc62d1e5
typeone28 <typeonethomas@gmail.com>
2025-07-28 18:00:10 -0500

    PC Sprites Added
    Initial right and left facing PC sprite png files and objects added.

---
+++ Danger-Jump/Assets/Scripts/RobeCharacterPC.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RobeCharacterPC : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Best for input
+    }
+
+    void FixedUpdate()
+    {
+        //Best for movement
+    }
+}
