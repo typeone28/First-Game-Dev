5fea4ac528de84b2e0a67fd682c7e32a8989dc8c
typeone28 <typeonethomas@gmail.com>
2025-07-29 14:41:31 -0500

    Gravity Scaled Up

--- Danger-Jump/Assets/Scripts/RobeCharacterPC.cs
+++ Danger-Jump/Assets/Scripts/RobeCharacterPC.cs
@@ -2,17 +2,28 @@ using UnityEngine;
 
 public class RobeCharacterPC : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb;
+    private Rigidbody2D body;
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float jump = 5f;
 
-    // Update is called once per frame
-    void Update()
+    //Start is called before the first frame update
+    private void Start()
     {
-        //Best for input
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame (Best for input)
+    private void Update()
+    {
+        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocityY);
+
+        // Loop to check for jump
+        if (Input.GetKey(KeyCode.Space))
+            body.linearVelocity = new Vector2(body.linearVelocityX, jump);
     }
 
     void FixedUpdate()
     {
-        //Best for movement
+        // Best for movement
     }
 }
